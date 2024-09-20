7ca2f038a0f47c24860dda0bfd9f8c5e17e8a74e
Alexey Melnikov <marvel12612@gmail.com>
2024-09-20 17:06:37 +0300

    renamed:    Feedback/feedback.html -> Feedback/feedback.php 	renamed:    Images/Wesker.jpg -> Images/0.jpg 	new file:   Images/1.jpg 	renamed:    Login/login.html -> Login/login.php 	renamed:    index.html -> index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 
 <head>
-    <title>Umbrella corp</title>
+    <? echo '<title>Umbrella corp</title>' ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +12,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sevillana&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <title>Главная</title>
 </head>
 <body>
     <header id="main-header">
@@ -21,8 +20,36 @@
         </h1>
 
         <ul class="lists" id="header-list">
-            <li><a href="Login\login.html">Авторизация</a></li>
-            <li><a href="Feedback\feedback.html">Фидбэк</a></li>
+            <li><a href="<?php
+                    $name='Авторизация';
+                    $link='Login\login.php';
+                    $current_page=false;
+
+                    echo $link;
+
+                    ?>"><?php
+
+                    if( $current_page )
+                        echo ' class="selected_menu"';
+                        echo $name;
+                    ?>
+                </a>
+            </li>
+            <li><a href="<?php
+                    $name='Фидбэк';
+                    $link='Feedback\feedback.php';
+                    $current_page=false;
+
+                    echo $link;
+
+                    ?>"><?php
+
+                    if( $current_page )
+                        echo ' class="selected_menu"';
+                        echo $name;
+                    ?>
+                </a>
+            </li>
         </ul>
     </header>
 
@@ -34,12 +61,23 @@
             <tr> <td>Umbrella Europe</td> <td>Европа</td> <td>До 1998</td> </tr>
             <tr> <td>Umbrella Industries</td> <td>-</td> <td>До 1998</td> </tr>
         </table>
+        <p>Наши основатели: 
+            <?php
+                $creators = array(
+                    "Освелл Э. Спенсер",
+                    "Джеймс Маркус",
+                    "Эдвард Эшфорд");
+                foreach ($creators as $n => $row) {
+                    echo $row . ', ';
+                }
+            ?>    
+        </p>
 
         <div id="main-photo">
             <p class="text">
                 Текущий руководитель: Альберт Вескер
             </p>
-            <img src="Images\Wesker.jpg" alt="" class="photo" id="umbrella-logo">
+            <? echo '<img src="Images\\'.(date(time()) % 2).'.jpg">' ?>
         </div>
     </main>
 
@@ -51,5 +89,9 @@
             <li>+7 (985) 899-07-18</li>
             <li>froykergames@gmail.com</li>
         </ul>
+        <?php
+        date_default_timezone_set('Europe/Moscow');
+        echo '<p>Сформировано '.date('m/d/Y G-i:s', time()).'</p>';
+        ?>
     </footer>
 </body>
