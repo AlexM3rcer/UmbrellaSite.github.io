666b76234cb7e80f4aa1b30672fa9fe9d69ecc2c
Alexey Melnikov <marvel12612@gmail.com>
2024-10-04 16:01:11 +0300

    new file:   Information/information.php 	new file:   Information/styleInformation.css 	new file:   Information/umbtellaCorporation1.jpg 	new file:   db.php modified:   index.php 	modified:   style.css

--- Information/information.php
+++ Information/information.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Подключение  -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styleInformation.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Sevillana&display=swap" rel="stylesheet">
@@ -16,13 +16,13 @@
 <body>
     <header id="main-header">
         <h1 id="menu">
-            <a href="index.php">Меню</a>
+            <a href="..\index.php">Меню</a>
         </h1>
 
         <ul class="lists" id="header-list">
             <li><a href="<?php
                     $name='Авторизация';
-                    $link='Login\login.php';
+                    $link='..\Login\login.php';
                     $current_page=false;
 
                     echo $link;
@@ -37,7 +37,7 @@
             </li>
             <li><a href="<?php
                     $name='Фидбэк';
-                    $link='Feedback\feedback.php';
+                    $link='..\Feedback\feedback.php';
                     $current_page=false;
 
                     echo $link;
@@ -52,7 +52,7 @@
             </li>
             <li><a href="<?php
                     $name='Информационный Центр';
-                    $link='Information\information.php';
+                    $link='information.php';
                     $current_page=false;
 
                     echo $link;
@@ -69,31 +69,37 @@
     </header>
 
     <main id="main-content">
-        <table id="table-corps">
-            <caption class="signature">Таблица филлиалов</caption>
-            <tr> <th>Филлиал</th> <th>Местоположение</th> <th>Учрежден</th> </tr>
-            <tr> <td>Paraguas Line</td> <td>Средиземное море</td> <td>Поздние 1980-е</td> </tr>
-            <tr> <td>Umbrella Europe</td> <td>Европа</td> <td>До 1998</td> </tr>
-            <tr> <td>Umbrella Industries</td> <td>-</td> <td>До 1998</td> </tr>
-        </table>
-        <p>Наши основатели: 
-            <?php
-                $creators = array(
-                    "Освелл Э. Спенсер",
-                    "Джеймс Маркус",
-                    "Эдвард Эшфорд");
-                foreach ($creators as $n => $row) {
-                    echo $row . ', ';
-                }
-            ?>    
-        </p>
 
-        <div id="main-photo">
-            <p class="text">
-                Текущий руководитель: Альберт Вескер
-            </p>
-            <? echo '<img src="Images\\'.(date(time()) % 2).'.jpg">' ?>
-        </div>
+        <?php
+            include "..\db.php";
+            $result = mysqli_query($mysql, "SELECT * FROM `terms`");
+        ?>
+
+        <form action="feedbacksend.php" method="POST" id="feedback-form" POST>
+
+            <label><input type="text" id="name-form" name="name">Термин</label>
+
+            <label><input type="text" id="name-form" name="name"> URL картинки</label>
+
+            <label><textarea name="text" id="textarea-form"></textarea>Описание</label>
+            
+
+            <input type="submit" id="submit-form" name="submit" value="Добавить">
+        </form>
+
+        <div id="termins">
+            <?php 
+                while($name = mysqli_fetch_assoc($result)){
+            ?>
+            <div class="term">
+                <img  title="<?php echo $name['name'];?>" src="..\Images\<?php echo $name['url'];?>"/>
+                <h2><?php echo $name['name'];?></h2>
+                <p><?php echo $name['description'];?><p>
+            </div>
+            <?php
+                }
+            ?>
+        </div>  
     </main>
 
     <footer id="info">
